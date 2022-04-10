304a1f8840afbad1efd8ca8b750045dc2e503e7c
gpallavi22 <103248963+gpallavi22@users.noreply.github.com>
2022-04-10 10:02:42 +0530

    This is my first commit with my original file

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int a,b;
+		
+		System.out.println("Enter the value of a :");
+		a = sc.nextInt();
+		
+		System.out.println("Enter the value of b :");
+		b = sc.nextInt();
+		
+		System.out.println("Addition of two number is "+(a+b));
+		System.out.println("Substraction of two number is "+(a-b));
+		System.out.println("Multiplication of two number is "+(a*b));
+		System.out.println("Division of two number is "+(a/b));
+		System.out.println("Modulus of two number is "+(a%b));
+
+
+	}
+
+}
